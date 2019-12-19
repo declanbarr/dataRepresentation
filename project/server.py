8966f88dc6ac8e63a7cdb4e74181e681a274b5a4
Declan Barr <deckybarr@googlemail.com>
2019-12-19 00:41:40 +0000

    Updated the server to import functions from productDAO

--- project/server.py
+++ project/server.py
@@ -1,54 +1,44 @@
 # Import Flask
 from flask import Flask, jsonify, request, abort
+from productDAO import productDAO
 
 # Creating an instance of the flask class
 app = Flask(__name__, static_url_path='', static_folder='.')
 
-products=[
-    {"id":1, "Product":"TV", "Brand":"Samsung", "Model":"Ru7100", "Price":399.84},
-    {"id":2, "Product":"TV", "Brand":"Philips", "Model":"32PHT4504/05", "Price":159.00},
-    {"id":3, "Product":"Laptop", "Brand":"ASUS", "Model":"E406MA", "Price":214.99},
-    {"id":4, "Product":"Printer", "Brand":"Brother", "Model":"DCP-J572DW", "Price":90.00},
-]
-nextId=5
 
 # curl "http://127.0.0.1:5000/products"
 # URL that will trigger the getAllProducts function
 @app.route('/products')
 # Function to return all products as a http response
 def getAllProducts():
-	return jsonify(products)
+    results = productDAO.getAll()
+    return jsonify(results)
 
 # curl "http://127.0.0.1:5000/products/1"
 # URL that will trigger the findProductById function
 @app.route('/products/<int:id>')
 # Function to return product by id as a http response
 def findProductById(id):
-    foundProducts = list(filter(lambda t: t['id'] == id, products))
-    # Return status code 204 if no content found
-    if len(foundProducts) == 0:
-        return jsonify ({}), 204
-
-    return jsonify(foundProducts[0])
+    foundProduct = productDAO.findByID(id)
+    return jsonify(foundProduct)
 
 # curl -i -H "Content-Type:application/json" -X POST -d "{\"Product\":\"newProduct\",\"Brand\":\"newBrand\",\"Model\":\"newModel\", \"Price\":99.99}" http://127.0.0.1:5000/products
 # URL that will trigger the create function
 @app.route('/products', methods=['POST'])
 # Function to create a new product
 def create():
-    global nextId
     # If server is unable to process request then abort with 400 message code
     if not request.json:
         abort(400)
     product = {
-        "id": nextId,
         "Product": request.json['Product'],
         "Brand": request.json['Brand'],
         "Model": request.json['Model'],
         "Price": request.json['Price']
     }
-    nextId += 1
-    products.append(product)
+    values =(product['Product'],product['Brand'],product['Model'],product['Price'])
+    newId = productDAO.create(values)
+    product['id'] = newId
     return jsonify(product)
 
 # curl -i -H "Content-Type:application/json" -X PUT -d "{\"Price\":1000}" http://127.0.0.1:5000/products/1
@@ -56,21 +46,19 @@ def create():
 @app.route('/products/<int:id>', methods=['PUT'])
 # Function to update a Product
 def update(id):
-    foundProducts = list(filter(lambda t: t['id']== id, products))
+    foundProduct = productDAO.findByID(id)
     # If there are no products found then abort with 404 message code
-    if (len(foundProducts) == 0):
+    if not foundProduct:
         abort(404)
     
-    foundProduct = foundProducts[0]
     # If server is unable to process request then abort with 400 message code
     if not request.json:
         abort(400)
-    
     reqJson = request.json
-    
     # If price is not an int then abort with 400 message code
     if 'Price' in reqJson and type(reqJson['Price']) is not int:
         abort(400)
+
     if 'Product' in reqJson:
         foundProduct['Product'] = reqJson['Product']
     if 'Brand' in reqJson:
@@ -79,21 +67,16 @@ def update(id):
         foundProduct['Model'] = reqJson['Model']
     if 'Price' in reqJson:
         foundProduct['Price'] = reqJson['Price']
-    
+    values = (foundProduct['Product'], foundProduct['Brand'], foundProduct['Model'], foundProduct['Price'])
+    productDAO.update(values)
     return jsonify(foundProduct)
 
 # curl -X DELETE "http://127.0.0.1:5000/products/1"
 # URL that will trigger the delete function
 @app.route('/products/<int:id>', methods=['DELETE'])
-# Function to return "In delete for id" as a http response
+# Function to delete record
 def delete(id):
-    # Create a list of products with the id passed to the function
-    foundProducts = list(filter(lambda t: t['id']==id, products))
-    # If there are no products found then abort with 404 message code
-    if (len(foundProducts) == 0):
-        abort(404)
-    # Remove the product
-    products.remove(foundProducts[0])
+    productDAO.delete(id)
     return jsonify({"done":True})
 
 if __name__ == '__main__':
